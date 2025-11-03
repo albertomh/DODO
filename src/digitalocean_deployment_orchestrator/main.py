0502b2fbba5256e337de3d235ddac102dc025e4c
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-11-03 20:04:47 +0000

    chore: nest package under a 'src/' directory

--- src/digitalocean_deployment_orchestrator/main.py
+++ src/digitalocean_deployment_orchestrator/main.py
@@ -10,7 +10,7 @@ configure_logging()
 def main():
     """Entrypoint for digitalocean_deployment_orchestrator."""
     logger = structlog.get_logger()
-    logger.info("digitalocean_deployment_orchestrator", version=f"v{version('digitalocean_deployment_orchestrator')}")
+    logger.info("DODO", version=f"v{version('digitalocean_deployment_orchestrator')}")
     return
 
 
