e39f1241f08a684181ec72adfbc06382f189c482
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-11-20 23:39:01 +0000

    feat: add IPAddressForDroplet to point to addresses in DNS records

--- src/digitalocean_deployment_orchestrator/types_cloudflare.py
+++ src/digitalocean_deployment_orchestrator/types_cloudflare.py
@@ -3,7 +3,10 @@
 from dataclasses import dataclass
 from typing import Literal, TypedDict
 
+from cloudflare.types.dns.ttl_param import TTLParam
+
 from digitalocean_deployment_orchestrator.types import EnvVarDataClass
+from digitalocean_deployment_orchestrator.types_DO import IPAddressForDroplet
 
 
 @dataclass(frozen=True)
@@ -12,7 +15,9 @@ class CloudflareCredentials(EnvVarDataClass):
 
 
 class DNSRecord(TypedDict):
-    zone_id: str
+    cf_zone_name: str
     type: Literal["A", "AAAA", "CNAME", "MX", "TXT", "NS", "SOA", "PTR"]
     name: str
+    content: str | IPAddressForDroplet
+    ttl: TTLParam | None  # in seconds - do not set if `proxied` is True
     proxied: bool
